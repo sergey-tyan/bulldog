58467b370a1f39c827b16fef2f837807b7e0ec6d
Сергей Тян <styan@sanscrit.kz>
2017-02-12 16:15:38 +0600

    add express server

--- app.js
+++ app.js
@@ -26,5 +26,18 @@ bot.on('inline_query', function onCallbackQuery(callbackQuery) {
 });
 
 
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res){
+    res.send('Bulldog bot here!');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
 
 
